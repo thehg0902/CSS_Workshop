2f628b545f25a01a4549b524808b6665f8293804
thehg0902 <thehg0902@gmail.com>
2021-02-21 18:11:58 -0500

    First

---
+++ script.js
@@ -0,0 +1,11 @@
+//scroll to top button
+function ScrollTop(){
+  window.scrollTo({
+      top:0,
+      behavior:"smooth"
+  })
+}
+//initiate button
+function Scroll(){
+document.getElementById('sec1').scrollIntoView({behavior:"smooth"});
+}
